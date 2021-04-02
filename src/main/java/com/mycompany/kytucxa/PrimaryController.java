d2926ee100ac598004901970fb7a4b9cedf8d127
nhatthanhnguyen610 <nhatthanhnguyen303@gmail.com>
2021-04-02 19:45:31 +0700

    a

--- src/main/java/com/mycompany/kytucxa/PrimaryController.java
+++ src/main/java/com/mycompany/kytucxa/PrimaryController.java
@@ -2,11 +2,27 @@ package com.mycompany.kytucxa;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private TextField userName;
+    @FXML
+    private PasswordField password;
+    
+    public void switchToSecondary() throws IOException {
+        if (userName.getText().equals("admin") && password.getText().equals("admin"))
+            App.setRoot("secondary");
+        else
+        {
+            Alert a = new Alert(Alert.AlertType.WARNING);
+            a.setContentText("Sai tên đăng nhập hoặc mật khẩu");
+            a.show();
+        }
+            
     }
+    
 }
