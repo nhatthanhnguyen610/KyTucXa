8c58673789ad026d022d6901138872bc9774afea
anhtuangl2k <52606237+anhtuangl2k@users.noreply.github.com>
2021-04-03 21:30:48 +0700

    sql

---
+++ src/main/java/com/dht/pojo/SinhVien.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dht.pojo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Admin
+ */
+public class SinhVien {
+    private int id;
+    private String name;
+    private String gioiTinh;
+    private Date ngaySinh;
+    private Date ngayNhanPhong;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the gioiTinh
+     */
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    /**
+     * @param gioiTinh the gioiTinh to set
+     */
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    /**
+     * @return the ngaySinh
+     */
+    public Date getNgaySinh() {
+        return ngaySinh;
+    }
+
+    /**
+     * @param ngaySinh the ngaySinh to set
+     */
+    public void setNgaySinh(Date ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    /**
+     * @return the ngayNhanPhong
+     */
+    public Date getNgayNhanPhong() {
+        return ngayNhanPhong;
+    }
+
+    /**
+     * @param ngayNhanPhong the ngayNhanPhong to set
+     */
+    public void setNgayNhanPhong(Date ngayNhanPhong) {
+        this.ngayNhanPhong = ngayNhanPhong;
+    }
+    
+    
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.mycompany.kytucxa {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql; 
 
     opens com.mycompany.kytucxa to javafx.fxml;
     exports com.mycompany.kytucxa;
+    exports com.dht.pojo;
 }
