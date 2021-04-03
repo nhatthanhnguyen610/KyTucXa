69433cac25fc85d16a8b10829a1a55f659864f72
nhatthanhnguyen610 <nhatthanhnguyen303@gmail.com>
2021-04-03 10:02:03 +0700

    giao dien

--- src/main/java/com/mycompany/kytucxa/PrimaryController.java
+++ src/main/java/com/mycompany/kytucxa/PrimaryController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class PrimaryController {
 
@@ -12,7 +14,19 @@ public class PrimaryController {
     private TextField userName;
     @FXML
     private PasswordField password;
+    @FXML
+    private HBox lbDangKy;
+    @FXML
+    private VBox lbDangNhap;
     
+    public void trangDangKy() throws IOException {
+        lbDangKy.setVisible(true);
+        lbDangNhap.setVisible(false);
+    }
+    public void trangDangNhap() throws IOException {
+        lbDangKy.setVisible(false);
+        lbDangNhap.setVisible(true);
+    }
     public void switchToSecondary() throws IOException {
         if (userName.getText().equals("admin") && password.getText().equals("admin"))
             App.setRoot("secondary");
--- src/main/java/com/mycompany/kytucxa/SecondaryController.java
+++ src/main/java/com/mycompany/kytucxa/SecondaryController.java
@@ -1,12 +1,39 @@
 package com.mycompany.kytucxa;
 
 import java.io.IOException;
+import java.util.Optional;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
 
 public class SecondaryController {
 
     @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+    private AnchorPane khuNam;
+    @FXML
+    private AnchorPane khuNu;
+    
+    public void danhsachNam() throws IOException{
+        khuNam.setVisible(true);
+        khuNu.setVisible(false);
+    }
+    public void danhsachNu() throws IOException{
+        khuNam.setVisible(false);
+        khuNu.setVisible(true);
+    }
+    
+    public void switchToPrimary() throws IOException {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Đăng xuất");
+        alert.setHeaderText("Bạn có chắc chắn muốn kết thúc phiên làm việc?");
+ 
+        // option != null.
+        Optional<ButtonType> option = alert.showAndWait();
+ 
+        if (option.get() == ButtonType.OK) {
+            App.setRoot("primary");
+        }
     }
 }
